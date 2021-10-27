f9634003d3821bc5e633227078c44b94a05dad17
Wilson Gearld Mead III <kem@rangoric.com>
2021-10-26 21:44:24 -0400

    updates and fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 
 const IndexPage = () => (
   <>
-    <SEO title="Wilson Gearld Mead III's Personal Site" />
+    <Seo title="Wilson Gearld Mead III's Personal Site" />
     <h1>Welcome To My Personal Site</h1>
     <p>I am a Professional Full Stack Developer, whatever that happens to mean nowadays.</p>
     <p>But I also like to write, and also love to game.</p>
-    <p>If you want to know me as a person, you want to read <Link to="/about-me/">About Me</Link><br/></p>
+    <p>If you want to know me as a person, you want to read <Link to="/about-me/">About Me</Link><br /></p>
     <p>If you want to work with me, you want my <Link to="/resume/">Resume</Link></p>
   </>
 )
