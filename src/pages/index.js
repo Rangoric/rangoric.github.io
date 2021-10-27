6d4a11e05bd1ecd62d7f46a0834b6fe9c762e8c8
Wilson Gearld Mead III <kem@rangoric.com>
2021-10-26 23:11:13 -0400

    Some new pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,13 @@ const IndexPage = () => (
     <Seo title="Wilson Gearld Mead III's Personal Site" />
     <h1>Welcome To My Personal Site</h1>
     <p>I am a Professional Full Stack Developer, whatever that happens to mean nowadays.</p>
-    <p>But I also like to write, and also love to game.</p>
+    <p>But I also like to write, and also love to game
+      <ul>
+        <li><Link to={"/base-srd/"}>Base SRD</Link></li>
+        <li><Link to={"/srd/"}>My SRD</Link></li>
+        <li><Link to={"/magic/"}>Magic Decks</Link></li>
+      </ul>
+    </p>
     <p>If you want to know me as a person, you want to read <Link to="/about-me/">About Me</Link><br /></p>
     <p>If you want to work with me, you want my <Link to="/resume/">Resume</Link></p>
   </>
