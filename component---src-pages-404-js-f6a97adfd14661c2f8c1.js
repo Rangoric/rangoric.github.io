c225689216708036d456e0eda99b419d03847d6e
Rangoric <kem@rangoric.com>
2023-01-11 01:49:44 +0000

    Deploying to master from @ Rangoric/rangoric.github.io@a369db3179efac5de32198ed186ebc54f520450a 🚀

---
+++ component---src-pages-404-js-f6a97adfd14661c2f8c1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrangoric_blog_and_website=self.webpackChunkrangoric_blog_and_website||[]).push([[883],{429:function(e,t,n){n.r(t);var a=n(7294),l=n(8183);t.default=()=>a.createElement(a.Fragment,null,a.createElement(l.Z,{title:"404: Not found"}),a.createElement("h1",null,"NOT FOUND"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-f6a97adfd14661c2f8c1.js.map
